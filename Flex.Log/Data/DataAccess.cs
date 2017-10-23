371610c877b1dd7b5d6b4033e54faa9fbb87d131
Omamestsarashvili <omamestsarashvili@ssa.gov.ge>
2017-10-23 18:38:40 +0400

    Testing asynchronous calls to API

---
+++ Flex.Log/Data/DataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flex.Log.Data
+{
+    class DataAccess
+    {
+        public async Task SendData()
+        {
+            var str = SendPostRequest();
+
+            Debug.WriteLine("postData:" + str);
+        }
+
+        public async Task SendPostRequest()
+        {
+            using (var client = new HttpClient())
+            {
+                var values = new List<KeyValuePair<string, string>>();
+                values.Add(new KeyValuePair<string, string>("variable", "hello"));
+                var content = new FormUrlEncodedContent(values);
+
+                  await client.PostAsync("http://localhost:24080/", content);
+                Debug.WriteLine("async");
+                
+            }
+        }
+    }
+}
---
+++ Flex.Log/Models/LogModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flex.Log.Models
+{
+    class LogModel
+    {
+        public static string ControllerName;
+        public static string MethodName;
+        public static DateTime RequestStartDate;
+        public static DateTime RequestEndDate;
+        public static long RequestPeriodTime;
+    }
+}
